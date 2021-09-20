8aa6d11cc07856cb0f31edf9677d6be900c5ccb2
Gleidson Leite <gledson.leytte@hotmail.com>
2021-09-20 11:15:48 -0300

    Message Listener

--- src/main/java/com/jms/App.java
+++ src/main/java/com/jms/App.java
@@ -1,13 +1,52 @@
 package com.jms;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        InitialContext context = new InitialContext();
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+        Connection connection = factory.createConnection();
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination queue = (Destination) context.lookup("financeiro");
+
+        MessageConsumer consumer = session.createConsumer(queue);
+
+        MessageListener messageListener = new MessageListener() {
+
+            @Override
+            public void onMessage(Message message) {
+                // TODO Auto-generated method stub
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    System.out.println(textMessage.getText());
+                } catch (JMSException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+        };
+        consumer.setMessageListener(messageListener);
+
+        new Scanner(System.in).nextLine();
+
+        session.close();
+        connection.close();
+        context.close();
     }
 }
